db23cdf74289e31ca3d6c96a62b6abafa840fbd8
ThainaPetry <49696402+ThainaPetry@users.noreply.github.com>
2019-07-03 15:51:12 -0300

    Add files via upload

---
+++ Trabalho002/Horario.java
@@ -0,0 +1,19 @@
+package HorarioClasses;
+
+public class Horario {
+	protected int hora = 9;
+	protected int minuto = 30;
+
+	public int getHora() {
+		return hora;
+	}
+	public void setHora(int hora) {
+		this.hora = hora;
+	}
+	public int getMinuto() {
+		return minuto;
+	}
+	public void setMinuto(int minuto) {
+		this.minuto = minuto;
+	}
+}
